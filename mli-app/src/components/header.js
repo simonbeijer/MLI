01de24a24e840537c41abf4108cc5d2449982265
Simon Beijer <beijersimon@gmail.com>
2020-07-04 18:10:40 +0200

    Removing default react, adding header component

---
+++ mli-app/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MLI</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
