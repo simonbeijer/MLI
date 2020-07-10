014d75b36d71cefbdbc72a1e6912a54e51413835
Simon Beijer <beijersimon@gmail.com>
2020-07-10 13:37:07 +0200

    Updates

---
+++ precache-manifest.5cff424b0b2f27c033ada54b06bbed46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a769bd6eba267e59d9efe591045232c",
+    "url": "/MLI/index.html"
+  },
+  {
+    "revision": "439ebc7288f830c94af6",
+    "url": "/MLI/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3b1757220a7f18583099",
+    "url": "/MLI/static/css/main.c082462c.chunk.css"
+  },
+  {
+    "revision": "439ebc7288f830c94af6",
+    "url": "/MLI/static/js/2.b9bc5319.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MLI/static/js/2.b9bc5319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1757220a7f18583099",
+    "url": "/MLI/static/js/main.914003cf.chunk.js"
+  },
+  {
+    "revision": "f3f01bc99053f961281c",
+    "url": "/MLI/static/js/runtime-main.cc80e862.js"
+  },
+  {
+    "revision": "6ec4d5070c9f86734dceafb6af2a1b2a",
+    "url": "/MLI/static/media/MLI-logo.6ec4d507.png"
+  }
+]);
