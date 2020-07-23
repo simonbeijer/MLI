3a608d97b7e13863f0ce429480ed10e08cd4dff3
Simon Beijer <beijersimon@gmail.com>
2020-07-23 18:21:49 +0200

    fixing navbar going over content

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -11,25 +11,24 @@ function Home() {
   const show = expanded ? "show": " ";
 
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light">
+    <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
       <Link className="navbar-brand" to="/"><img src={logo} alt="mli-logo" className="logo" /></Link>
       <button className="navbar-toggler" type="button" onClick={ toggleExpand }>
         <span className="navbar-toggler-icon" ></span>
       </button>
-      <div className={"collapse navbar-collapse " + show }>
-
-        <ul className="navbar-nav ml-auto">
+      <div className={"collapse navbar-collapse  " + show }>
+        <ul className="navbar-nav ml-auto ">
           <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
+            <Link onClick={toggleExpand} className="nav-link" to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="about">About</Link>
+            <Link onClick={toggleExpand} className="nav-link" to="about">About</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="products">Products</Link>
+            <Link onClick={toggleExpand} className="nav-link" to="products">Products</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="contact">Contact</Link>
+            <Link onClick={toggleExpand} className="nav-link" to="contact">Contact</Link>
           </li>
         </ul>
       </div>
